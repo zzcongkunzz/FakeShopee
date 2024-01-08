290e6f1bb57b0ff592510579fd1c85f3e2b6555a
zzcongkunzz <dangcong200502@gmail.com>
2024-01-08 22:52:18 +0700

    create MyDbContext

---
+++ FakeShopee/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FakeShopee.Models;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext (DbContextOptions options) : base(options) {}
+    
+    #region DbSet
+    
+
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- FakeShopee/Program.cs
+++ FakeShopee/Program.cs
@@ -1,8 +1,14 @@
+using FakeShopee.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MyDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("MyDb"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
