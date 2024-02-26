ddac1c19660f4bcbce114514c4925c46e7044e32
zzcongkunzz <dangcong200502@gmail.com>
2024-02-27 00:16:08 +0700

    Create the product detail page interface

---
+++ FakeShopee/Controllers/ProductDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FakeShopee.Controllers;
+
+public class ProductDetailsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
