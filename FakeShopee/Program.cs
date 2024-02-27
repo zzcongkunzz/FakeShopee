81d59abbe0894502cbd69e6a0fd5b02c8f256137
zzcongkunzz <dangcong200502@gmail.com>
2024-02-27 11:18:37 +0700

    feature display data

--- FakeShopee/Program.cs
+++ FakeShopee/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=ProductDetails}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
