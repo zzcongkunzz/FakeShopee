0f706572719af5d788c160eade240930c1318d94
zzcongkunzz <dangcong200502@gmail.com>
2024-02-27 11:22:10 +0700

    fix text

--- FakeShopee/Program.cs
+++ FakeShopee/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=LoginAndSignup}/{action=Index}/{id?}");
 
 app.Run();
