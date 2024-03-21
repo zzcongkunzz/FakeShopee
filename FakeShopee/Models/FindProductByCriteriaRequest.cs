c13211638e0bbf19dc54521899221346c6b8cdd9
zzcongkunzz <dangcong200502@gmail.com>
2024-03-22 01:07:38 +0700

    add Page Total + method Clone

--- FakeShopee/Models/FindProductByCriteriaRequest.cs
+++ FakeShopee/Models/FindProductByCriteriaRequest.cs
@@ -2,11 +2,17 @@
 
 namespace FakeShopee.Models;
 
-public class FindProductByCriteriaRequest
+public class FindProductByCriteriaRequest : ICloneable
 {
     public String NameProductOrCategory { get; set; }
     public String Category { get; set; }
     public SortType SortBy { get; set; } = SortType.POPULAR;
     public int PageNumber { get; set; } = 1;
 
+    public int PageTotal { get; set; } = 1;
+
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
 }
