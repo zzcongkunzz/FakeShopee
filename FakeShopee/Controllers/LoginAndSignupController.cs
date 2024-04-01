d180755b690866872d6f5c04fecc8f53c7ffc9bf
zzcongkunzz <dangcong200502@gmail.com>
2024-04-02 02:16:56 +0700

    feature login + register

--- FakeShopee/Controllers/LoginAndSignupController.cs
+++ FakeShopee/Controllers/LoginAndSignupController.cs
@@ -2,8 +2,10 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using FakeShopee.constant;
 using FakeShopee.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
@@ -26,13 +28,24 @@ public class LoginAndSignupController : Controller
     {
         return View();
     }
+    
+    [HttpGet]
+    [Route("/LoginAndSignup/Login/{id:guid}")]
+    public async Task<IActionResult> Login(Guid id)
+    {
+        Users users = MyDbContext.Users.Find(id);
+        return View(users);
+    }
 
     [HttpPost]
     public async Task<IActionResult> Login (String username, String password)
     {
-        Users users = MyDbContext.Users
+        Users user = MyDbContext.Users
             .FirstOrDefault(u => (u.Email.Equals(username) || u.PhoneNumber.Equals(username)) && u.Password.Equals(password));
-        return Ok(users);
+        
+        HttpContext.Session.SetString("User", JsonConvert.SerializeObject(user));
+        
+        return Ok(user);
 
     }
 
@@ -43,8 +56,21 @@ public class LoginAndSignupController : Controller
 
     [HttpPost]
     public async Task<IActionResult> Register([FromBody] Users user){
+        Users userExist = MyDbContext.Users
+            .FirstOrDefault(u => u.Email.Equals(user.Email) || u.PhoneNumber.Equals(user.PhoneNumber));
         
-        return Ok(user);
+        if (userExist != null)
+        {
+            return Ok(Empty);
+        }
+        else
+        {
+            List<UserGroup> userGroup = MyDbContext.UserGroups.ToList();
+            user.UserGroup = userGroup.Find(item => item.Name.Equals(RoleType.CUSTOMER.GetDescription()));
+            MyDbContext.Users.Add(user);
+            MyDbContext.SaveChanges();
+            return Ok(user);
+        }
     }
     
  
